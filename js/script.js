60756a5ac49305b7b3156e222b59cc43b34bb0d1
joyablue <dev.joyable@gmail.com>
2021-11-19 10:32:26 +0900

    210926 2차 수정 완료
    메뉴 구현 (디테일 수정 필요)

--- js/script.js
+++ js/script.js
@@ -17,4 +17,19 @@ $(document).ready(function(){
 			nextEl: '.sw-next-btn',
 		},
 	});
+
+	 //.depth1 바로 밑에 있는 li 를 저장
+	 var lis = $('.depth1 > li');
+
+	 $.each(lis, function(index, itme) {
+ 
+		 $(this).mouseenter(function() {
+			 $(this).find('.depth2').stop().show();
+		 });
+ 
+		 $(this).mouseleave(function() {
+			 $(this).find('.depth2').stop().hide();
+		 });
+ 
+	 });
 })
